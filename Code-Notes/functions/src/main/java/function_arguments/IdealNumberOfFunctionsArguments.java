2c8db1d0eb21829080965bd915a7cbe141c2155e
George Alan <georgealanrufo@gmail.com>
2023-02-18 16:53:50 -0300

    Continue in page 78

--- Code-Notes/functions/src/main/java/function_arguments/IdealNumberOfFunctionsArguments.java
+++ Code-Notes/functions/src/main/java/function_arguments/IdealNumberOfFunctionsArguments.java
@@ -1,7 +1,8 @@
 package function_arguments;
 
-import java.io.IOException;
 import java.io.InputStream;
+
+
 
 public class IdealNumberOfFunctionsArguments {
     /*
@@ -43,8 +44,43 @@ public class IdealNumberOfFunctionsArguments {
 
 
     /* AVOID FUNCTIONS WITH FLAG ARGUMENTS
-
+    Flag arguments are ugly. Passing a boolean into a function is a truly terrible practice. It
+    immediately complicates the signature of the method, loudly proclaiming that this function
+    does more than one thing. It does one thing if the flag is true and another if the flag is false!
      */
+
+    /* Prefer exceptions to returning error codes
+    Try/catch blocks are ugly in their own right. They confuse the structure of the code and
+    mix error processing with normal processing. So it is better to extract the bodies of the try
+    and catch blocks out into functions of their own.
+     */
+    private void logError(Exception e) {}
+    public void delete(Object page) {
+        try {
+            deletePageAndAllReferences(page);
+        } catch (Exception e) {
+            logError(e);
+        }
+    }
+
+    private void deletePageAndAllReferences(Object page) throws Exception {
+        deletePage(page);
+        deleteReference(page);
+    }
+
+    private void deleteReference(Object name) {}
+    private void deletePage(Object page) {}
+
+    /*
+    In the above, the delete function is all about error processing. It is easy to understand
+    and then ignore. The deletePageAndAllReferences function is all about the processes of
+    fully deleting a page. Error handling can be ignored. This provides a nice separation that
+    makes the code easier to understand and modify.
+     */
+
+
+
+
 }
 
-// TODO Continue in page 41, Flag Arguments
+// TODO Continue in page 78, Vertical Openness Between Concepts
